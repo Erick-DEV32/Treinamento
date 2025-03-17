ffc4e4f08ec5bd7ab7962196eab6aa9270361dff
Erick ramon <erickramon8483@gmail.com>
2025-03-17 19:36:46 -0300

    Projetos de métodos.

---
+++ Aula34_Quinto_Projeto/Aula33_Metodos/Aula33_Metodos/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Aula33_Metodos
+{
+    class Program
+    {
+        // tipo de acesso do metodo _____ Modificador_de_metodo tipo de retorno (Parametro_de_metodos)
+        public static double soma(double a, double b)
+        {
+            double SomaResul = a + b;
+            return SomaResul;
+            // return (a + b); apenas outra forma para apresentar. 
+        }
+
+        public static void ImprimiMensagens()
+        {
+            Console.WriteLine("Não existi informação no metodos.");
+            Console.WriteLine("Não tem informação de entrada ou saída. ");
+        }
+        //Modificadores de parâmetros de entradas. 
+        /*
+         Ref: sinaliza que os parâmetros de entradas "PODE" ser modificado pelo metodos
+        Out: Sinaliza que os parâmetros de entrada "DEVE" ser modificado pelo método
+         */
+        public static void multiplica (double a, double b, ref double resultado)
+        {
+            double ResultadoMult = a * b;
+            resultado = ResultadoMult;
+        }
+        public static void multiplica2(double a, double b, out double resultado)
+        {
+            double ResultadoMult = a * b;
+            resultado = ResultadoMult;
+        }
+
+        static void Main(string[] args)
+        {
+            //Chamando metodis
+            double resul = soma(100, 101);
+            Console.WriteLine("O resultado é " + resul);
+            Console.WriteLine("Digite dois números");
+            double Teste1 = Convert.ToDouble(Console.ReadLine());
+            double Teste2 = Convert.ToDouble(Console.ReadLine());
+            double Resultado = soma(Teste1, Teste2);
+            Console.WriteLine("O resultado dos dois numeros somados é " + Resultado);
+
+            ImprimiMensagens();
+            double numero1 = 10;
+            double numero2 = 20;
+            double resultadomultiplica = 0;
+            multiplica(numero1, numero2, ref resultadomultiplica);
+            Console.WriteLine("Seu resultado é " + resultadomultiplica);
+            multiplica2(numero1, numero2, out resultadomultiplica);
+            Console.WriteLine("O Resultado é: " + resultadomultiplica);
+        }
+    }
+}
---
+++ Aula34_Quinto_Projeto/Aula34_Quinto_Projeto/Aula34_Quinto_Projeto/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula34_Quinto_Projeto
+{
+    class Program
+    {       //Treinando métodos.
+        public struct Dados
+        {
+            public string nome;
+            public DateTime Data_Nacimento;
+            public string Nome_da_Rua;
+            public UInt32 Numero_Casa; 
+
+        }
+        public enum resultado
+        {
+            sucesso = 0,
+            sair = 1,
+            Excecao = 2
+        }
+        public static void MostraMensagem (String Mensagem)
+        {
+            Console.WriteLine(Mensagem);
+            Console.WriteLine("Pressione qualquer tecla para sair..");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static resultado PegaString(ref string MinhaString, string mensagem)
+        {
+            resultado retorno;
+            Console.WriteLine(mensagem);
+            string temp = Console.ReadLine();
+            if (temp == "s" || temp == "S")
+            {
+                retorno = resultado.sair;
+            }
+            else
+            {
+                MinhaString = temp;
+                retorno = resultado.sucesso;
+                Console.Clear();
+                return retorno;
+            }
+            Console.Clear();
+            return retorno;
+        }
+
+
+        public static resultado Data (ref DateTime data, string mensagem)
+        {
+            resultado retorno;
+            do
+            {
+                try
+                {
+                    Console.WriteLine(mensagem);
+                    string temp = Console.ReadLine();
+                    if (temp == "s" || temp == "S")
+                    {
+                        retorno = resultado.sair;
+                    }
+                    else
+                    {
+                        data = Convert.ToDateTime(temp);
+                        retorno = resultado.sucesso;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("EXECÇÃO: " + e.Message);
+                    Console.WriteLine("Pressione qualquer tecla");
+                    Console.ReadKey();
+                    Console.Clear();
+                    retorno = resultado.Excecao;
+                }
+
+            } while (retorno == resultado.Excecao);
+            Console.Clear();
+            return retorno;
+
+        }
+        public static resultado PegaUint32(ref UInt32 numeroUint32, string mensagem)
+        {
+            resultado retorno;
+            do
+            {
+                try
+                {
+                    Console.WriteLine(mensagem);
+                    string temp = Console.ReadLine();
+                    if (temp == "s" || temp == "S")
+                    {
+                        retorno = resultado.sair;
+                    }
+                    else
+                    {
+                        numeroUint32 = Convert.ToUInt32(temp);
+                        retorno = resultado.sucesso;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("EXECÇÃO: " + e.Message);
+                    Console.WriteLine("Pressione qualquer tecla");
+                    Console.ReadKey();
+                    Console.Clear();
+                    retorno = resultado.Excecao;
+                }
+
+            } while (retorno == resultado.Excecao);
+            Console.Clear();
+            return retorno;
+
+        }
+        public static void CadastroUsuario (ref List <Dados> ListaUsuario)
+        {
+            Dados CadastroUsuario;
+            CadastroUsuario.nome = "";
+            CadastroUsuario.Nome_da_Rua = "";
+            CadastroUsuario.Numero_Casa = 0;
+            CadastroUsuario.Data_Nacimento = new DateTime();
+            if (PegaString(ref CadastroUsuario.nome, "Digite o nome completo ou digite S para sair.") != resultado.sucesso)
+                return;
+            if (Data(ref CadastroUsuario.Data_Nacimento, "Digite a data de nascimento no formato DD/MM/AAAA ou digite S para sair") != resultado.sucesso)
+                return;
+            if (PegaString(ref CadastroUsuario.Nome_da_Rua, "Digite o nome da rua ou S para sair") != resultado.sucesso)
+                return;
+            if (PegaUint32(ref CadastroUsuario.Numero_Casa, "Digite o numero da casa ou S para sair") != resultado.sucesso)
+                return;
+            ListaUsuario.Add(CadastroUsuario);
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<Dados> ListaDeUsuario = new List<Dados>();
+            string opcao = "";
+            do
+            {
+                Console.WriteLine("Digite C para cadastrar um novo usuário e S para sair.");
+                opcao = Console.ReadKey(true).KeyChar.ToString().ToLower();
+                if (opcao == "c")
+                {
+                    CadastroUsuario(ref ListaDeUsuario);
+                }
+                else if (opcao == "s")
+                {
+                    MostraMensagem("encerrando o programa");
+                }
+                else
+                {
+                    MostraMensagem("opção desconecida");
+                }
+            } while (opcao !="s");
+
+        }
+    }
+}
